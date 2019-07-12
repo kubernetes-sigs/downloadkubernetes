b5401b2ad87a497523b2b9c7c3c3e6a6766ef2c3
chuckha <ha.chuck@gmail.com>
2019-07-12 09:07:10 -0400

    es6 and fix a click bug
    Signed-off-by: chuckha <ha.chuck@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,45 +5,36 @@ require('./mystyles.scss');
 });
 
 function eventListener(kind) {
-    document.getElementById(kind).addEventListener("click", function(evt){
-        let hideClass = kind + "-hide";
-        let k = evt.target.dataset[kind];
-        let notSelector = "tbody tr:not(."+k+")";
-        let selector = "tbody tr."+k;
+    let buttonGroupQuery = '#' + kind + ' > .button';
+    let buttonGroup = document.querySelectorAll(buttonGroupQuery);
 
-        var rows;
+    let rowsQuery = 'tbody tr';
+    let rows = document.querySelectorAll(rowsQuery);
 
-        // if this is the actively selected one then clear stuff
-        if (evt.srcElement.classList.contains("is-success")) {
-            evt.srcElement.classList.remove("is-success")
-            rows = document.querySelectorAll(notSelector);
-            for (let i=0; i < rows.length; i++) {
-                rows.item(i).classList.remove(hideClass)
-            }
-            return
-        }
+    let hideClass = kind + "-hide";
 
-        for (let i=0; i < this.children.length; i++) {
-            if (this.children.item(i).dataset[kind] === evt.srcElement.dataset[kind]) {
-                evt.srcElement.classList.add("is-success");
-                continue
-            }
-            this.children.item(i).classList.remove("is-success");
-        }
-        rows = document.querySelectorAll(notSelector);
-        for (let i=0; i < rows.length; i++) {
-            rows.item(i).classList.add(hideClass)
-        }
-        rows = document.querySelectorAll(selector);
-        for (let i=0; i < rows.length; i++) {
-            rows.item(i).classList.remove(hideClass)
-        }
+    buttonGroup.forEach(button => {
+        console.log(button);
+        button.addEventListener('click', (evt) => {
+            let buttonData = button.dataset[kind];
+            buttonGroup.forEach(b => {
+                b.classList.remove('is-success');
+            })
+            button.classList.add('is-success');
+            rows.forEach(row => {
+                if (row.classList.contains(buttonData)) {
+                    row.classList.remove(hideClass);
+                } else {
+                    row.classList.add(hideClass);
+                }
+            });
+        });
     });
 }
 
-let links = document.getElementsByClassName("copy");
-for (let i=0; i < links.length; i++) {
-    links.item(i).addEventListener('click', function(evt) {
+// make the click link work
+document.querySelectorAll(".copy").forEach(link => {
+    link.addEventListener('click', (evt) => {
         evt.preventDefault();
 
         let el = document.createElement("textarea");
@@ -55,6 +46,6 @@ for (let i=0; i < links.length; i++) {
         el.select();
         document.execCommand("copy");
         document.body.removeChild(el);
+        return false;
     });
-}
-
+});
