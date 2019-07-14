2b9895e3076680739c496502557a90e4aa734da9
chuckha <ha.chuck@gmail.com>
2019-07-14 12:38:30 -0400

    Fix header
    Signed-off-by: chuckha <ha.chuck@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,5 @@ USER=root
 HOSTNAME=ubuntu-s-1vcpu-1gb-nyc3-01
 HOST_IP=$(doctl compute droplet list "${HOSTNAME}"  --format PublicIPv4 --no-header)
 
+# TODO: add a retry here, this restart command hangs a lot.
 ssh "${USER}"@"${HOST_IP}" 'service downloadkubernetes restart'
